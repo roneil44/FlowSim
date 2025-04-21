ad3b1d1bb045c944d628e837e91f29046e9bb165
unknown <rileyponeil@yahoo.com>
2025-04-20 21:17:36 -0700

    Added 2 advection solvers update

--- node.py
+++ node.py
@@ -2,15 +2,20 @@
 
 import typing
 
+## Define a Node that is the representation of a single point on a grid. It stores all relevant info about
+# the fluid at the point given. This includes velocity components, pressure, density, viscosity, location, and mesh id
+# Currently this is equip to work on a 2D plane 
 class Node:
 
-    def __init__(self, location:tuple, pressure: float, temperature: float, density:float, viscosity:float, velocity:tuple):
+    def __init__(self, location:tuple, pressure: float, density:float, viscosity:float, velocity:tuple, mesh_id:int):
         self.x_location = location[0]
         self.x_velocity = velocity[0]
         self.y_location = location[1]
         self.y_velocity = velocity[1]
         self.pressure = pressure
-        self.temperature = temperature
         self.pressure = pressure
         self.viscosity = viscosity
+        self.mesh_id = mesh_id
+        
+        
     
