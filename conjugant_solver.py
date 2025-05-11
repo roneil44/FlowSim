db5593ab35ec74c3bbdf75541d7d56a2740e4031
roneil44 <rileyponeil@yahoo.com>
2025-05-11 11:43:16 -0700

    Conjugant solve working

--- conjugant_solver.py
+++ conjugant_solver.py
@@ -7,15 +7,69 @@
 
 # Imports
 import numpy as np
+import numpy.typing as npt
 
 def conjugant_solve(A:list[list], x0:list, b:list) -> list:
     ''' This function takes an initial guess x0, and the right hand side values
     b and solves Ax = b iteratively using the conjugant gradient method'''
 
-    pass
+    # Convert to numpy arrays to make vector operations easier
+    A = np.array(A)
+    x0 = np.array(x0)
+    b= np.array(b)
+
+    #Solver conditions
+    max_iters = 1000
+    i = 0
+    eps = .01
+
+    #First iteration
+    r = b - Ax(A, x0)
+    d = r
+    
+    delta_new = np.dot(r, r)
+    delta_0 = delta_new
 
 
-def Ax(A:list[list], x:list) -> list:
+    while i<=max_iters and delta_new > eps*delta_0:
+        
+        q = Ax(A, d)
+        alpha = delta_new / np.dot(d, q)
+        x0 = x0 + alpha*d
+
+        if i % 50 == 0:
+            r = b - Ax(A, x0)
+        else:
+            r = r - alpha*q
+
+        delta_old = delta_new
+        delta_new = np.dot(r, r)
+        beta = delta_new / delta_old
+
+        d = r+beta*d
+
+        i+=1
+        
+        # # Find beta with old and new g
+        # beta = -(np.dot(g_new, g_new)) / np.dot(r, r)
+        
+        # #Update g and x0 guess
+        # r = g_new
+        # x0 = x_new
+        
+        # # Rest of solver
+        # d = r - beta*d
+        # q = Ax(A, d)
+        # alpha = (np.dot(r, r)) / (np.dot(r, q))
+        # x_new = x0 + alpha*d
+        # g_new = r - alpha*q
+
+        # i += 1
+
+    return(x0)
+
+
+def Ax(A:npt.ArrayLike, x:npt.ArrayLike) -> npt.NDArray:
     '''This function solves for A*x used in the conjugant gradient solver. For testing 
     the conjugant gradient solver a full A matrix is defined. In the future this can 
     be simplified an all sparse values can be removed'''
--- conjugant_test.py
+++ conjugant_test.py
@@ -10,8 +10,37 @@ from conjugant_solver import *
 #Initializations
 x0 = [2,1]
 b = [3,2]
-A = [[1,8],[5,1]]
+A = [[5,1],[1,8]]
 
 
 # Test Ax
 print(Ax(A, x0))
+
+# Run conjugant solve
+solution = conjugant_solve(A, x0, b)
+print(solution)
+
+# Direct solve with numpy matrix solver
+real = np.linalg.solve(A, b)
+print(real)
+
+# Test with larger matrix
+# First generate symmetric positive matrix
+A1 = [[5, 2, 1, 7],[1, 3, 8, 4],[5, 2, 9, 3],[4, 2, 6, 2]]
+A2 = np.transpose(A1)
+A = np.matmul(A1, A2)
+print(A)
+
+# Solution vector
+b = [1, 4, 6, 7]
+
+#Initial guess
+x0 = [0, 0, 0, 0]
+
+#Conjugant gradient solve
+solution= conjugant_solve(A, x0 ,b)
+print(solution)
+
+#Direct solve using numpy library
+real = np.linalg.solve(A, b)
+print(real)
